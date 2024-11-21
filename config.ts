da5f9ee21f78acba4991ca16bfd696a32baa6e4f
Sri Tikkireddy <sri.tikkireddy+github@databricks.com>
2024-11-21 11:47:06 -0500

    format changes

--- config.ts
+++ config.ts
@@ -7,49 +7,49 @@ const outputDir = path.join("outputs", currentDate);
 
 // Ensure the output directory exists
 if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
+  fs.mkdirSync(outputDir, { recursive: true });
 }
 
 const maxPagesToCrawl = parseInt(process.env.MAX_PAGES_TO_CRAWL ?? "500", 10);
 
 export const defaultConfig: Config[] = [
-    {
-        url: "https://docs.databricks.com/en/machine-learning/index.html",
-        match: "https://docs.databricks.com/en/generative-ai/agent-framework/**",
-        maxPagesToCrawl: maxPagesToCrawl,
-        outputFileName: path.join(outputDir, "generative-ai-content.json"),
-        maxTokens: 2000000,
-    },
-    {
-        url: "https://docs.databricks.com/en/machine-learning/index.html",
-        match: [
-            "https://docs.databricks.com/en/mlflow/**",
-            "https://docs.databricks.com/en/machine-learning/manage-model-lifecycle/**",
-            "https://docs.databricks.com/en/machine-learning/mlops/**"
-        ],
-        maxPagesToCrawl: maxPagesToCrawl,
-        outputFileName: path.join(outputDir, "mlflow.json"),
-        maxTokens: 2000000,
-    },
-    {
-        url: "https://docs.databricks.com/en/machine-learning/foundation-models/retired-models-policy.html",
-        match: [
-            "https://docs.databricks.com/en/machine-learning/foundation-models/**",
-        ],
-        maxPagesToCrawl: maxPagesToCrawl,
-        outputFileName: path.join(outputDir, "foundation-model-policy.json"),
-        maxTokens: 2000000,
-    },
-    {
-        url: "https://docs.databricks.com/en/machine-learning/serve-models.html",
-        match: [
-            "https://docs.databricks.com/en/generative-ai/**",
-            "https://docs.databricks.com/en/ai-gateway/**",
-            "https://docs.databricks.com/en/machine-learning/model-serving/**",
-            "https://docs.databricks.com/en/machine-learning/model-inference/**",
-        ],
-        maxPagesToCrawl: maxPagesToCrawl,
-        outputFileName: path.join(outputDir, "model-serving.json"),
-        maxTokens: 2000000,
-    },
+  {
+    url: "https://docs.databricks.com/en/machine-learning/index.html",
+    match: "https://docs.databricks.com/en/generative-ai/agent-framework/**",
+    maxPagesToCrawl: maxPagesToCrawl,
+    outputFileName: path.join(outputDir, "generative-ai-content.json"),
+    maxTokens: 2000000,
+  },
+  {
+    url: "https://docs.databricks.com/en/machine-learning/index.html",
+    match: [
+      "https://docs.databricks.com/en/mlflow/**",
+      "https://docs.databricks.com/en/machine-learning/manage-model-lifecycle/**",
+      "https://docs.databricks.com/en/machine-learning/mlops/**",
+    ],
+    maxPagesToCrawl: maxPagesToCrawl,
+    outputFileName: path.join(outputDir, "mlflow.json"),
+    maxTokens: 2000000,
+  },
+  {
+    url: "https://docs.databricks.com/en/machine-learning/foundation-models/retired-models-policy.html",
+    match: [
+      "https://docs.databricks.com/en/machine-learning/foundation-models/**",
+    ],
+    maxPagesToCrawl: maxPagesToCrawl,
+    outputFileName: path.join(outputDir, "foundation-model-policy.json"),
+    maxTokens: 2000000,
+  },
+  {
+    url: "https://docs.databricks.com/en/machine-learning/serve-models.html",
+    match: [
+      "https://docs.databricks.com/en/generative-ai/**",
+      "https://docs.databricks.com/en/ai-gateway/**",
+      "https://docs.databricks.com/en/machine-learning/model-serving/**",
+      "https://docs.databricks.com/en/machine-learning/model-inference/**",
+    ],
+    maxPagesToCrawl: maxPagesToCrawl,
+    outputFileName: path.join(outputDir, "model-serving.json"),
+    maxTokens: 2000000,
+  },
 ];
